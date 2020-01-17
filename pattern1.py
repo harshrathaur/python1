938d65d9b8c3a79a8946fd885d1357a91c0355d3
harshrathaur <55021326+harshrathaur@users.noreply.github.com>
2020-01-17 19:29:06 +0530

    Update pattern1.py

--- pattern1.py
+++ pattern1.py
@@ -1,11 +1,8 @@
-def pattern(n):
-    x = 0
-    for i in range(0, n):
-        x += 1
-    for j in range(0, i + 1):
-        print(x, end=" ")
-        print("\r")
-pattern(5)
+n=5
+for num in range(n + 1):
+    for i in range(num):
+        print(num, end=" ")
+    print("\r")
 
 
 
